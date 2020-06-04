b16fe4bab9473576df26d51d137b88440b64d668
Caro <cfouquet.web@gmail.com>
2020-06-05 01:27:38 +0200

    JSON process OK, HTML render OK, Random product OK

--- index.py
+++ index.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests, json
-from flask import Flask, render_template,request
+import requests, json, random
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 ##Page accueil##
 @app.route('/')
 def home():
-    return render_template("home.html", message = "Bienvenue dans le jeux du juste prix, entrer un prix")
+    message = "Bienvenue dans le jeux du juste prix, entrer un prix"
+    return show_render(message, try_list)
 
-def json_api():
+
+def show_render(message, try_list):
+    name = product_infos[0]
+    image = product_infos[1]
+    return render_template("home.html", name=name, image=image, message=message, try_list=try_list)
+
+
+def json_api(keyword, max_price):
     # global price
-
     url = "https://api.cdiscount.com/OpenApi/json/Search"
     params = {
         "ApiKey": "23b7aa67-4725-4f50-bdb5-a3b3a70bc33c",
         "SearchRequest": {
-            "Keyword": "tablette",
+            "Keyword": keyword,
             "SortBy": "relevance",
             "Pagination": {
                 "ItemsPerPage": 5,
@@ -27,41 +34,49 @@ def json_api():
             "Filters": {
                 "Price": {
                     "Min": 0,
-                    "Max": 400
+                    "Max": max_price
                 },
                 "Navigation": "computers",
                 "IncludeMarketPlace": "false",
             }
         }
     }
-
     r = requests.post(url, data=json.dumps(params))
-    price = json.loads(r.text)['Products'][0]['BestOffer']['SalePrice']
+    random_index = random.randint(0,len(json.loads(r.text))-1)
+    price = json.loads(r.text)['Products'][random_index]['BestOffer']['SalePrice']
+    image = json.loads(r.text)['Products'][random_index]['MainImageUrl']
+    name = json.loads(r.text)['Products'][random_index]['Name']
 
-    print(r.text)
-    print(price)
+    return name, image, price
 
-    return price
+
+
+some_keywords = ["tablette", "tv", "television", "livre", "smartphone", "four", "cd", "casque", "clavier"]
+random_keyword = some_keywords[random.randint(0, len(some_keywords)-1)]
+random_max_price = random.randint(0, 499)
+product_infos = json_api(random_keyword, random_max_price)
+try_list = []
 
 
 ##Jeux du juste Prix##
 @app.route('/', methods=['POST'])
 def just_price():
-    p = json_api()
-    print("p =" +p)
+    price_object = int(float(product_infos[2]))
 
     if request.method == 'POST':
-        nb = request.form['rep_user']
-
-        price_user = float(nb)
-        price_object = float(p)
+        price_user = int(request.form['rep_user'])
 
         if price_user < price_object:
-            return render_template("home.html", message = "C'est plus !")
+            message = "C'est plus !"
+            try_list.append(str(price_user) + " : " + message)
+            return show_render(message, try_list)
         elif price_user > price_object:
-            return render_template("home.html", message="C'est moins !")
+            message = "C'est moins !"
+            try_list.append(str(price_user) + " : " + message)
+            return show_render(message, try_list)
         elif price_user == price_object:
-            return render_template("home.html", message="Bravo")
+            message = "Bravo, c'est bien : " + str(price_user) + " €"
+            return show_render(message, try_list)
 
     return render_template("home.html")
 
